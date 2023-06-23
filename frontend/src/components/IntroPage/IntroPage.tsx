5f643f6045eb32b9a72da75ef0bc54f5599ac53e
Miiyu <71932936+miiyu-fujita@users.noreply.github.com>
2023-06-23 18:23:17 -0400

    Setup frontend app and add intro page content

---
+++ frontend/src/components/IntroPage/IntroPage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./IntroPage.css";
+import Button from "@mui/material/Button";
+
+function IntroPage() {
+  return (
+    <div className="intro-page-container">
+      <div className="intro-page-header-container">
+        <h1 className="intro-page-header">Name of the App</h1>
+        <h3 className="intro-page-subheader">
+          Let's fight food insecurity <span>together</span>.
+        </h3>
+      </div>
+
+      <Button className="intro-page-register-button" variant="contained">
+        Join Now
+      </Button>
+      <h4 className="intro-page-login-prompt">
+        Already have an account?{" "}
+        <span className="intro-page-login-prompt-link">Login</span>
+      </h4>
+    </div>
+  );
+}
+
+export default IntroPage;
