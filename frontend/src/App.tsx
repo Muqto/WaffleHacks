ae12450e3f550b3ad18b10cbdaf7693f55d2e09e
Miiyu <71932936+miiyu-fujita@users.noreply.github.com>
2023-06-23 18:55:58 -0400

    Add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import IntroPage from "./components/IntroPage/IntroPage";
+import LoginPage from "./components/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <IntroPage />
+      <LoginPage />
     </div>
   );
 }
