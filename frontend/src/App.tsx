6cb6b3958db724e100c8571d5d1c7df56a68da81
Muqto <muqto14@gmail.com>
2023-06-23 23:04:01 -0400

    created barcode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import HomePage from "./components/HomePage/HomePage";
 import IntroPage from "./components/IntroPage/IntroPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import SignUpPage from "./components/SignUpPage/SignUpPage";
+import BarcodePopup from "./components/BarcodePopup/BarcodePopup";
+import BarcodeScanner from "./components/BarcodeScanner/BarcodeScanner";
+import PostScanPage from "./components/BarcodeScanner/PostScanPage/PostScanPage";
 
 function App() {
   return (
@@ -11,7 +14,11 @@ function App() {
       {/* <IntroPage /> */}
       {/* <LoginPage /> */}
       {/* <SignUpPage /> */}
-      <HomePage />
+      {/* <HomePage /> */}
+      {/* <BarcodePopup/> */}
+      {/* <BarcodeScanner/> */}
+      <PostScanPage/>
+      {/* <BottomNavigation /> */}
     </div>
   );
 }
