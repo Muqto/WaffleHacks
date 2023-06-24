8097652aa64792a588a25c69d556e2027281e6e8
Muqto <muqto14@gmail.com>
2023-06-24 18:40:35 -0400

    done profile page

--- frontend/src/components/ProfilePage/ProfilePage.jsx
+++ frontend/src/components/ProfilePage/ProfilePage.jsx
@@ -1,16 +1,27 @@
 import { Avatar, Button } from "@mui/material";
-import React from "react";
+import {useContext} from "react";
 import "./ProfilePage.css";
+import { Context } from "../../context/context";
+import { useNavigate } from "react-router";
+import RestaurantVerticalCard from "../RestaurantVerticalCard/RestaurantVerticalCard";
 
 function ProfilePage() {
+  const {user, allOwners} = useContext(Context)
+  const navigate = useNavigate()
   return (
     <div className="profile-page-container">
       <div className="profile-page-header">
-        <Avatar className="profile-page-avatar" />
+        <Avatar src={user.profilePicture} className="profile-page-avatar" />
       </div>
       <div className="profile-bottom-div">
-        <h1 className="profile-page-username">John Wick</h1>
+        <h1 className="profile-page-username">{user.username}</h1>
+        <div className="profile-page-logout">
+        <Button onClick={() => navigate("/login")} className="profile-page-logout-button" variant="outlined">
+          Logout
+        </Button>
       </div>
+      </div>
+      
     </div>
   );
 }
