6f63c4db580ef213fd5f7b2de90c2ea5b8bb6163
Miiyu <71932936+miiyu-fujita@users.noreply.github.com>
2023-06-23 22:16:34 -0400

    Add home page restaurant cards

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import RestaurantHorizontalCard from "../RestaurantHorizontalCard/RestaurantHorizontalCard";
 import RestaurantVerticalCard from "../RestaurantVerticalCard/RestaurantVerticalCard";
 import "./HomePage.css";
 
@@ -9,13 +10,20 @@ function HomePage() {
       <h2 className="home-page-message">Hey John 👋</h2>
       <h3 className="home-page-your-restaurants">Your restaurants</h3>
       <div className="home-page-your-restaurants-card-carousel">
-        <RestaurantVerticalCard />
-        <RestaurantVerticalCard />
-        <RestaurantVerticalCard />
+        <RestaurantVerticalCard restaurantName="McDucknalds" points={95} />
+        <RestaurantVerticalCard restaurantName="noodles" points={30} />
+        <RestaurantVerticalCard restaurantName="Snoopy's" points={25} />
+        <RestaurantVerticalCard restaurantName="Naruto's Ramen" points={105} />
       </div>
       <h3 className="home-page-discover-restaurants">
         Discover <span className="home-page-discover-emoji">👀</span>
       </h3>
+      <div className="home-page-discover-restaurants-container">
+        <RestaurantHorizontalCard restaurantName="A" numberOfStars={4} />
+        <RestaurantHorizontalCard restaurantName="B" numberOfStars={5} />
+        <RestaurantHorizontalCard restaurantName="C" numberOfStars={2} />
+        <RestaurantHorizontalCard restaurantName="D" numberOfStars={5} />
+      </div>
     </div>
   );
 }
--- frontend/src/components/RestaurantVerticalCard/RestaurantVerticalCard.tsx
+++ frontend/src/components/RestaurantVerticalCard/RestaurantVerticalCard.tsx
@@ -1,9 +1,29 @@
+import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 import "./RestaurantVerticalCard.css";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-function RestaurantVerticalCard() {
+interface IRestaurantVerticalCardProps {
+  restaurantName: string;
+  points: number;
+}
+function RestaurantVerticalCard(props: IRestaurantVerticalCardProps) {
   return (
-    <div className="restaurant-vertical-card-container">Restaurant Card</div>
+    <div className="restaurant-vertical-card-container">
+      <Avatar className="restaurant-vertical-card-restaurant-icon" />
+      <IconButton size="small" className="subscribed-restaurant-icon-button">
+        <CheckCircleIcon className="subscribed-restaurant-icon" />
+      </IconButton>
+      <h4 className="restaurant-vertical-card-restaurant-name">
+        {props.restaurantName}
+      </h4>
+      <h1 className="restaurant-vertical-card-restaurant-points">
+        {props.points}{" "}
+        <span className="restaurant-vertical-card-restaurant-points-text">
+          points
+        </span>
+      </h1>
+    </div>
   );
 }
 
