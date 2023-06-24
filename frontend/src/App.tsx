5968738d12e0c785f22c4d127877824d50e9f6ee
Miiyu <71932936+miiyu-fujita@users.noreply.github.com>
2023-06-23 21:02:16 -0400

    Set up Home Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
 import IntroPage from "./components/IntroPage/IntroPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import SignUpPage from "./components/SignUpPage/SignUpPage";
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <IntroPage /> */}
       {/* <LoginPage /> */}
-      <SignUpPage />
+      {/* <SignUpPage /> */}
+      <HomePage />
     </div>
   );
 }
--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import RestaurantVerticalCard from "../RestaurantVerticalCard/RestaurantVerticalCard";
+import "./HomePage.css";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="home-page-container">
+      <h3 className="app-logo">MunchPoints</h3>
+      <h2 className="home-page-message">Hey John 👋</h2>
+      <h3 className="home-page-your-restaurants">Your restaurants</h3>
+      <div className="home-page-your-restaurants-card-carousel">
+        <RestaurantVerticalCard />
+        <RestaurantVerticalCard />
+        <RestaurantVerticalCard />
+      </div>
+      <h3 className="home-page-discover-restaurants">
+        Discover <span className="home-page-discover-emoji">👀</span>
+      </h3>
+    </div>
+  );
 }
 
 export default HomePage;
