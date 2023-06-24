e2f569782f9510d5e2e234174f1ed13c1718eeb0
Muqto <muqto14@gmail.com>
2023-06-23 23:57:54 -0400

    barcosde

--- frontend/src/components/BarcodeScanner/PostScanPage/PostScanPage.tsx
+++ frontend/src/components/BarcodeScanner/PostScanPage/PostScanPage.tsx
@@ -7,12 +7,12 @@ const PostScanPage = () => {
         <div className="post-scan-page-header">
             <Avatar className="post-scan-page-avatar"/>
         </div>
-        <div className="post-scan-page-username">
-            <Typography>Hello John</Typography>
-        </div>
+        <h1 className="post-scan-page-username">John Wick</h1>
+        <h2 className="post-scan-page-points">150 Points</h2>
+        <p className="post-scan-page-p">Choose to redeem user's current points or to add points</p>
         <div className="post-scan-page-buttons">
-            <Button variant="contained">Redeem Points</Button>
-            <Button variant="contained">Add Points</Button>
+            <Button className="post-scan-page-button1" variant="contained">Give Discount</Button>
+            <Button className="post-scan-page-button2" variant="contained">Send Points</Button>
         </div>
     </div>
   )
