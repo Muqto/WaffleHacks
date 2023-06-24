2221732a264c1f7af7dfd2414bdc965584f7d1be
Muqto <muqto14@gmail.com>
2023-06-24 00:50:50 -0400

    context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomNavigation from "./components/BottomNavigation/BottomNavigation";
 import HomePage from "./components/HomePage/HomePage";
@@ -8,18 +8,22 @@ import SignUpPage from "./components/SignUpPage/SignUpPage";
 import BarcodePopup from "./components/BarcodePopup/BarcodePopup";
 import BarcodeScanner from "./components/BarcodeScanner/BarcodeScanner";
 import PostScanPage from "./components/BarcodeScanner/PostScanPage/PostScanPage";
+import { Context } from "./context/context";
 
 function App() {
+  const [slide, setSlide] = useState("")
   return (
     <div className="App">
+      <Context.Provider value={{slide, setSlide}}>
       {/* <IntroPage /> */}
       {/* <LoginPage /> */}
       {/* <SignUpPage /> */}
       {/* <HomePage /> */}
-      {/* <BarcodePopup/> */}
+      <BarcodePopup/>
       {/* <BarcodeScanner/> */}
-      <PostScanPage/>
-      {/* <BottomNavigation /> */}
+      {/* <PostScanPage/> */}
+      <BottomNavigation />
+      </Context.Provider>
     </div>
   );
 }
